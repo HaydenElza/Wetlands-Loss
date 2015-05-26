9111d107d2b031ef689efe6fdd0949a3ca9a935f
Hayden Elza <hayden.elza@gmail.com>
2015-05-26 14:14:07 -0500

    Added Skeleton
    Added skeleton for the Monte Carlo Simulation. Added .gitignore. Added
    db_design and package_manuals folders to gitignore.

---
+++ monte_carlo_simulation.R
@@ -0,0 +1,25 @@
+# Load packages
+  install.packages("rgdal")
+  library("rgdal")
+  install.packages("maptools")
+  library("maptools")
+  install.packages("rgeos")
+  library("rgeos")
+  install.packages("raster")
+  library("raster")
+
+
+# Set working directory
+  setwd("e:/Hayden_Elza/Wetlands-Loss/")
+  
+# Read in shapefiles for current and historical wetlands
+  
+# Introduce random error within dataset's spatial accuracy
+  
+# Select by location: within/outside
+  
+# Break up data according to enumeration units: North/South Ecoregions, per County
+  
+# Point Sampling
+  
+# Export summary tables
