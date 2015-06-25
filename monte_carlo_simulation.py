43ab3878814cadcb6df75a322b37ee4327521877
HaydenElza <hayden.elza@gmail.com>
2015-06-25 11:25:42 -0500

    Added resources.md, monte_carlo_simulation.py can now process two layers and determine loss.

--- monte_carlo_simulation.py
+++ monte_carlo_simulation.py
@@ -1,4 +1,175 @@
-import ogr
-import gdal
+# Import ogr and gdal depending on system
+try:
+	import ogr, osr, gdal
+except:
+	try:
+		from osgeo import ogr, osr, gdal
+	except: 
+		print "Import of ogr/gdal failed."
+
+# Import other needed modules
+import numpy, os, sys, shutil, gdalconst
 
 
+# Set working directory
+path = os.path.dirname(__file__)
+
+# Set paths for datasets
+pls_path = os.path.join(path, "data/test_area/pls_wgs.shp")
+wwi_path = os.path.join(path, "data/test_area/wwi_wgs_dissolve.shp")
+
+
+# Addition of normally distributed error to data as a part of the Monte Carlo simulation
+def error_band(dataset,geom,stdev):  # geom: point or polygon, stdev: the standard deviation of the dataset's spatial accuracy
+	for feature in layer:
+		pass
+	return
+
+# Determine whether PLS points are within or outside of WWI polygons
+def in_out(pls,wwi):
+	return
+
+
+#----------------------------------------------------------------------------
+# Main
+#----------------------------------------------------------------------------
+
+#------------------
+# Prepare PLS data
+#------------------
+
+# Check if file exists
+if not os.path.isfile(pls_path):
+	print "File does not exist."
+
+# Get appropriate driver
+driver = ogr.GetDriverByName('ESRI Shapefile')
+
+# Open the file using the driver
+pls_source = driver.Open(pls_path, gdalconst.GA_ReadOnly)
+
+# Verify if the file was opened, if not exit
+if pls_source is None:
+	print 'Failed to open file'
+	sys.exit(-1)
+
+# Get first layer
+pls_layer = pls_source.GetLayer(0)
+
+# Get one feature to get geom ref later
+pls_feature = pls_layer.GetFeature(0)
+
+# Feature count
+featureCount1 = pls_layer.GetFeatureCount()
+print featureCount1
+
+
+"""# Get feature
+feature2 = pls_layer.GetFeature(0)
+# Get feature geometry
+poly = feature2.GetGeometryRef()
+print poly
+# Get access to the layer's non-spatial info: number of fields, field names, field types, etc.
+featureDefn2 = pls_layer.GetLayerDefn()
+print featureDefn2
+# Get feature count
+fieldCount2 = featureDefn2.GetFieldCount()
+print fieldCount2"""
+
+#------------------
+# Prepare WWI data
+#------------------
+
+# Check if file exists
+if not os.path.isfile(wwi_path):
+	print "File does not exist."
+
+# Open the file using the driver
+wwi_source = driver.Open(wwi_path, gdalconst.GA_ReadOnly)
+
+# Verify if the file was opened, if not exit
+if wwi_source is None:
+	print 'Failed to open file'
+	sys.exit(-1)
+
+# Get first layer
+wwi_layer = wwi_source.GetLayer(0)
+
+# Get one feature to get geom ref later
+wwi_feature = wwi_layer.GetFeature(0)
+
+# Feature count
+featureCount2 = wwi_layer.GetFeatureCount()
+print featureCount2
+
+#---------------------------
+# Prepare intersection data
+#---------------------------
+
+# Create temp folder
+temp_dir = os.path.join(path,"temp/")
+if not os.path.exists(temp_dir):
+	os.makedirs(temp_dir)
+else:
+	print temp_dir,"already exits. Please remove or rename."
+
+# Create shapefile
+dstshp = driver.CreateDataSource(os.path.join(temp_dir,"temp.shp"))
+pls_notlost = dstshp.CreateLayer('foolayer',geom_type=ogr.wkbPoint)
+if pls_notlost is None:
+	print "Could not create output layer."
+	sys.exit(-1)
+
+pls_notlost_def = pls_notlost.GetLayerDefn() # Every feature in layer will have this
+
+#----------------
+# Intoduce error
+#----------------
+
+#---------------------------------
+# PLS within/outside WWI wetlands
+#---------------------------------
+
+TrueCount = 0
+FalseCount = 0
+# Iterate through point features to find where points intersect WWI
+for i in range(0, featureCount1):
+	# Get PLS feature and set geom refs
+	point_feature = pls_layer.GetFeature(i)
+	point = point_feature.GetGeometryRef()
+	poly = wwi_feature.GetGeometryRef()
+	# Check if WWI intersects PLS point
+	cross = poly.Intersects(point)
+	# Count the number of intersects or nots
+	geometry = point_feature.GetGeometryRef()
+	if cross:
+		TrueCount = TrueCount + 1
+		# Add current PLS point to file
+		pls_notlost_feature = ogr.Feature(pls_notlost_def) #create feature
+		pls_notlost_feature.SetGeometry(geometry)  		   #add geometry
+		pls_notlost_feature.SetFID(i) 			           #set id
+		pls_notlost.CreateFeature(pls_notlost_feature) 	   #add feature to layer
+	else:
+		FalseCount = FalseCount + 1
+
+# Diagonostic count
+print "Wetlands not lost:", TrueCount
+print "Wetlands lost:",FalseCount
+print "Total wetlands:",TrueCount+FalseCount
+
+
+#---------------------------
+# Combine and export tables
+#---------------------------
+
+
+# Free memory
+wwi_source = None
+wwi_layer = None
+pls_source = None
+pls_layer = None
+dstshp = None
+pls_notlost = None
+pls_notlost_feature = None
+
+#shutil.rmtree(temp_dir)
---
+++ resources.md
@@ -0,0 +1,53 @@
+Resources for python script:
+- [Extract point from raster](http://pthelma.readthedocs.org/en/latest/dev/spatial.html#spatial.extract_point_from_raster)
+	
+	~~~
+	def extract_point_from_raster(point, data_source, band_number=1):
+    """Return floating-point value that corresponds to given point."""
+
+    # Convert point co-ordinates so that they are in same projection as raster
+    point_sr = point.GetSpatialReference()
+    raster_sr = osr.SpatialReference()
+    raster_sr.ImportFromWkt(data_source.GetProjection())
+    transform = osr.CoordinateTransformation(point_sr, raster_sr)
+    point.Transform(transform)
+
+    # Convert geographic co-ordinates to pixel co-ordinates
+    x, y = point.GetX(), point.GetY()
+    forward_transform = Affine.from_gdal(*data_source.GetGeoTransform())
+    reverse_transform = ~forward_transform
+    px, py = reverse_transform * (x, y)
+    px, py = int(px + 0.5), int(py + 0.5)
+
+    # Extract pixel value
+    band = data_source.GetRasterBand(band_number)
+    structval = band.ReadRaster(px, py, 1, 1, buf_type=gdal.GDT_Float32)
+    result = struct.unpack('f', structval)[0]
+    if result == band.GetNoDataValue():
+        result = float('nan')
+    return result
+    ~~~
+
+- [Python – Extract raster data value at a point (*simple*)](https://waterprogramming.wordpress.com/2014/10/07/python-extract-raster-data-value-at-a-point/)
+	
+	~~~
+	from __future__ import division
+	from osgeo import gdal
+	from geopy.geocoders import Nominatim
+	 
+	def get_value_at_point(rasterfile, pos):
+	  gdata = gdal.Open(rasterfile)
+	  gt = gdata.GetGeoTransform()
+	  data = gdata.ReadAsArray().astype(np.float)
+	  gdata = None
+	  x = int((pos[0] - gt[0])/gt[1])
+	  y = int((pos[1] - gt[3])/gt[5])
+	  return data[y, x]
+	 
+	city = 'Ithaca, NY'
+	g = Nominatim().geocode(city, timeout=5)
+	p = (g.longitude,g.latitude)
+	print get_value_at_point('path/to/my/raster/file', p)
+	~~~
+
+- 
