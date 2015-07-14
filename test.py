ec73866ae2b729a247711abf9b3bc0b2333dab7d
HaydenElza <hayden.elza@gmail.com>
2015-07-13 19:17:10 -0500

    Added monte carlo multi processing version

--- test.py
+++ test.py
@@ -2,15 +2,16 @@ import multiprocessing,random
 
 def worker(num):
 	"""thread worker function"""
-	x = random.randint(1,10000)
+	x = random.randint(1,100)
 	print 'Worker:', num ,"\t",
 	for y in range(0,x): print y,
+	print var1
 	print ""
 	return
 
 print "Before main"
 
-
+var1 = "Variable 1"
 jobs = []
 for i in range(10):
 	p = multiprocessing.Process(target=worker, args=(i,))
